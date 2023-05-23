cc7bc46a223a7860553e71340485a00b637165c2
bunsung92 <bunsung92@naver.com>
2023-05-24 00:37:53 +0900

    상품 등록 폼 & 상품 등록 처리 - `@ModelAttribute` #1

--- src/main/java/hello/itemservice/web/basic/BasicItemController.java
+++ src/main/java/hello/itemservice/web/basic/BasicItemController.java
@@ -4,12 +4,14 @@ import hello.itemservice.domain.Item.Item;
 import hello.itemservice.domain.Item.ItemRepository;
 import java.util.List;
 import javax.annotation.PostConstruct;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("/basic/items")
@@ -31,9 +33,69 @@ public class BasicItemController {
     }
 
     @GetMapping("/{itemId}")
-    public String item(@PathVariable long itemId, Model model) {
+    public String item(
+        @PathVariable
+        long itemId,
+        Model model) {
         Item item = itemRepository.findById(itemId);
         model.addAttribute("item", item);
+
+        return "basic/item";
+    }
+
+    @GetMapping("/add")
+    public String addForm() {
+        return "basic/addForm";
+    }
+
+//    @PostMapping("/add")
+    public String save(@RequestParam String itemName,
+                       @RequestParam int price,
+                       @RequestParam Integer quantity,
+                       Model model) {
+
+        Item item = new Item();
+
+        item.setItemName(itemName);
+        item.setPrice(price);
+        item.setQuantity(quantity);
+
+        itemRepository.save(item);
+
+        model.addAttribute("item", item);
+
+        return "basic/item";
+    }
+
+//    @PostMapping("/add")
+    public String addItemV2(@ModelAttribute("item") Item item) {
+        itemRepository.save(item);
+
+        //ModelAttribute의 name은 무엇인가?
+        //model.addAttribute("item", item); 해당 코드를 실행 해 주는 결과를 가지고 있다.
+
+        return "basic/item";
+    }
+
+//    @PostMapping("/add")
+    public String addItemV3(@ModelAttribute Item item) {
+        itemRepository.save(item);
+
+        // ModelAttribute의 name을 지우면?
+        // Default 설정은 Item -> item 으로 박아준다 결과론 적으로 클래스의 이름에서 첫 글자를 소문자로 바꿔서 넣어주는 것이다.
+        // 따라서 model.addAttribute("item", item); 해당 코드를 실행 해 주는 결과를 가지고 있다.
+
+        return "basic/item";
+    }
+
+    @PostMapping("/add")
+    public String addItemV4(Item item) {
+        // ModelAttribute를 지우면?
+        // 역시 Item -> item 으로 치환해서 모델에 가지고 있다. 고로 위에서 작동한 동작과 같다.
+        // 주의할 점은 영한 햄은 보다 명시해 주는걸 선호한다.
+        itemRepository.save(item);
+
+
 
         return "basic/item";
     }
